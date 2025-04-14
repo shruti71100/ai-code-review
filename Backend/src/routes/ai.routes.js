0e0a312d1f96f01e4807d471b7ba63ddcff19e9c
shruti71100 <shrutikumari71100@gmail.com>
2025-04-14 13:58:09 +0530

    Update ai.routes.js

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const aiController=require("../Controllers/ai.controllers")
 const router=express.Router();
 
-router.post("/get-review",aiController.getReview);
+router.post("https://ai-code-review-backend-kv4w.onrender.com",aiController.getReview);
 module.exports=router;
 
 /*ai.routes.js – The Door (Routes Layer)
